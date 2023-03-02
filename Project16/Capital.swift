450ebc98a14f74f691a18da5696ff9902ae4f2c4
VladimirVinageras <vvinageras@gmail.com>
2023-03-02 11:00:21 +0200

    Add files via upload

---
+++ Project16/Capital.swift
@@ -0,0 +1,25 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Vladimir Vinageras on 23.02.2023.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+    
+    
+
+}
